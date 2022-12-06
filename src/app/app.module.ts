44163c48b0662f6b3df112c1178c59a52e01ce62
sufailbadushat <sufailbadushat@gamil.com>
2022-12-06 17:54:12 +0530

    route added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,23 @@ import { AppComponent } from './app.component';
 import { AddCourseComponent } from './add-course/add-course.component';
 import { ViewCourseComponent } from './view-course/view-course.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RouterModule, Routes } from '@angular/router';
+
+let myRoute:Routes=
+[
+  {
+    path:"",
+    component:ViewCourseComponent
+  },
+  {
+    path:"view",
+    component:ViewCourseComponent
+  },
+  {
+    path:"add",
+    component:AddCourseComponent
+  }
+]
 
 @NgModule({
   declarations: [
@@ -16,7 +33,8 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
